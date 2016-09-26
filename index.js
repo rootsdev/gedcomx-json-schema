9c2931dc75e6146c35fd5ee7782faf524a4f63d8
Justin <justincyork@gmail.com>
2016-09-26 21:17:06 +0000

    group gedcomx data types

--- index.js
+++ index.js
@@ -146,30 +146,6 @@ module.exports = {
         attribution: { $ref: '#/definitions/Attribution' }
       })
     },
-    DisplayProperties: {
-      type: 'object',
-      properties: {
-        name: { type: 'string' },
-        gender: { type: 'string' },
-        lifespan: { type: 'string' },
-        birthDate: { type: 'string' },
-        birthPlace: { type: 'string' },
-        deathDate: { type: 'string' },
-        deathPlace: { type: 'string' },
-        marriageDate: { type: 'string' },
-        marriagePlace: { type: 'string' },
-        ascendancyNumber: { type: 'string' },
-        descendancyNumber: { type: 'string' },
-        familiesAsParent: {
-          type: 'array',
-          items: { $ref: '#/definitions/FamilyView' }
-        },
-        familiesAsChild: {
-          type: 'array',
-          items: { $ref: '#/definitions/FamilyView' }
-        }
-      }
-    },
     Event: {
       type: 'object',
       properties: merge(SubjectProperties, {
@@ -210,17 +186,6 @@ module.exports = {
           items: { $ref: '#/definitions/Qualifier' }
         }
       })
-    },
-    FamilyView: {
-      type: 'object',
-      properties: {
-        parent1: { $ref: '#/definitions/ResourceReference' },
-        parent2: { $ref: '#/definitions/ResourceReference' },
-        children: {
-          type: 'array',
-          items: { $ref: '#/definitions/ResourceReference' }
-        }
-      }
     },
     GedcomX: {
       type: 'object',
@@ -274,25 +239,6 @@ module.exports = {
             { type: 'array', items: { type: 'string' } }
           ]
         }
-      }
-    },
-    Link: {
-      type: 'object',
-      properties: {
-        rel: { type: 'string' },
-        href: { type: 'string' },
-        template: { type: 'string' },
-        type: { type: 'string' },
-        accept: { type: 'string' },
-        allow: { type: 'string' },
-        hreflang: { type: 'string' },
-        title: { type: 'string' }
-      }
-    },
-    Links: {
-      type: 'object',
-      patternProperties: {
-        ".*": { $ref: '#/definitions/Link' }
       }
     },
     Name: {
@@ -476,6 +422,62 @@ module.exports = {
       properties: {
         lang: { type: 'string' },
         value: { type: 'string' }
+      }
+    },
+    
+    // GedcomX RS
+    DisplayProperties: {
+      type: 'object',
+      properties: {
+        name: { type: 'string' },
+        gender: { type: 'string' },
+        lifespan: { type: 'string' },
+        birthDate: { type: 'string' },
+        birthPlace: { type: 'string' },
+        deathDate: { type: 'string' },
+        deathPlace: { type: 'string' },
+        marriageDate: { type: 'string' },
+        marriagePlace: { type: 'string' },
+        ascendancyNumber: { type: 'string' },
+        descendancyNumber: { type: 'string' },
+        familiesAsParent: {
+          type: 'array',
+          items: { $ref: '#/definitions/FamilyView' }
+        },
+        familiesAsChild: {
+          type: 'array',
+          items: { $ref: '#/definitions/FamilyView' }
+        }
+      }
+    },
+    Link: {
+      type: 'object',
+      properties: {
+        rel: { type: 'string' },
+        href: { type: 'string' },
+        template: { type: 'string' },
+        type: { type: 'string' },
+        accept: { type: 'string' },
+        allow: { type: 'string' },
+        hreflang: { type: 'string' },
+        title: { type: 'string' }
+      }
+    },
+    Links: {
+      type: 'object',
+      patternProperties: {
+        ".*": { $ref: '#/definitions/Link' }
+      }
+    },
+    FamilyView: {
+      type: 'object',
+      properties: {
+        parent1: { $ref: '#/definitions/ResourceReference' },
+        parent2: { $ref: '#/definitions/ResourceReference' },
+        children: {
+          type: 'array',
+          items: { $ref: '#/definitions/ResourceReference' }
+        }
       }
     },
     
